dfe59d6563dfc9d0941afc2b1eed34298e846bdc
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-01-14 17:19:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ['https://hospital-chi-bay.vercel.app',"http://localhost:3000"]);
+  res.setHeader('Access-Control-Allow-Origin','https://hospital-chi-bay.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
