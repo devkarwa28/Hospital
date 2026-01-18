796cd32ace08dbe1281a768bcdd874f6ec32bbcb
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-01-18 20:17:12 +0530

    Update AdminDashboard.js

--- client/src/Admin/AdminDashboard.js
+++ client/src/Admin/AdminDashboard.js
@@ -1,52 +1,52 @@
-import React, { useContext, useEffect, useState } from 'react'
-import adminstyles from './admin.module.css'
-import AdminSidebar from './AdminSidebar'
-import { Outlet, useNavigate } from 'react-router-dom'
-import { store } from '../App'
-import axios from 'axios'
+import React, { useContext, useEffect, useState } from "react";
+import adminstyles from "./admin.module.css";
+import AdminSidebar from "./AdminSidebar";
+import { Outlet, useNavigate } from "react-router-dom";
+import { store } from "../App";
+import axios from "axios";
 const AdminDashboard = () => {
-    const API = process.env.REACT_APP_API_URL;
-    const [data, setData] = useState("")
-    const { token } = useContext(store);
-    const navigate = useNavigate();
-    useEffect(()=>{
-        axios.get(`${API}/admindashboard`,{
-            headers:{
-                "x-token" : token,
-            }
-        })
-        .then((res)=>{
-            setData(res.data);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    });
-    if(!token)
-    {
-        navigate('/admin')
+  const API = process.env.REACT_APP_API_URL;
+  const [data, setData] = useState("");
+  const { token } = useContext(store);
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get(`${API}/admindashboard`, {
+        headers: {
+          "x-token": token,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    if (!token) {
+      navigate("/admin");
     }
-    return (
-        <>
-            <section className={adminstyles.bread}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <p>{data}Admin Dashboard</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <main className='d-flex'>
-                <aside className={adminstyles.sidebar}>
-                    <AdminSidebar/>
-                </aside>
-                <div className={adminstyles.outlet}>
-                    <Outlet/>
-                </div>
-            </main>
-        </>
-    )
-}
+  });
+  return (
+    <>
+      <section className={adminstyles.bread}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <p>{data}Admin Dashboard</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <main className="d-flex">
+        <aside className={adminstyles.sidebar}>
+          <AdminSidebar />
+        </aside>
+        <div className={adminstyles.outlet}>
+          <Outlet />
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
