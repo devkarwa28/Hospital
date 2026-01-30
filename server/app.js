5f3906b3b816cb04c372136aef883c4b84e866b3
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-01-30 23:25:18 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,35 +5,30 @@ const treatmentRouting = require("./router/treatmentRouting");
 const offerRouting = require("./router/offerRouting");
 const doctorRouting = require("./router/doctorRouting");
 const appointmentRouting = require("./router/appointmentRouting");
-const contactRouting = require("./router/contactRouting")
+const contactRouting = require("./router/contactRouting");
 
 let app = express();
-require("./db/dbconfig")
+require("./db/dbconfig");
 
-// ✅ CORS — MUST be FIRST
-app.use(cors({
-  origin: "https://hospital-chi-bay.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-token"],
-  credentials: true
-}));
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin','https://hospital-chi-bay.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+//CORS — Cross Orgin Resource Sharing
+app.use(
+  cors({
+    origin: "https://hospital-chi-bay.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-token"],
+    credentials: true,
+  }),
+);
 
-// ... your routes
+//routes
 
 app.use(express.json());
-app.use("/",signupRouting);
-app.use("/",treatmentRouting);
-app.use("/",offerRouting);
-app.use("/",doctorRouting);
-app.use("/",appointmentRouting);
-app.use("/",contactRouting)
+app.use("/", signupRouting);
+app.use("/", treatmentRouting);
+app.use("/", offerRouting);
+app.use("/", doctorRouting);
+app.use("/", appointmentRouting);
+app.use("/", contactRouting);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
